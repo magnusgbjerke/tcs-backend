b4f9b00f5d517ac01b542122af85b1cca6aca2f7
magnusgbjerke <magnusgbjerke@gmail.com>
2025-06-12 20:26:01 +0200

    update description

--- src/main/java/com/tcs/onlinestore/swagger/Description.java
+++ src/main/java/com/tcs/onlinestore/swagger/Description.java
@@ -55,6 +55,7 @@ public interface Description {
             * **204 - No Content**
             * **400 - Bad Request**
             * **404 - Not Found**
+            * **409 - Conflict**
             * **500 - Internal Error**
             """;
 }
